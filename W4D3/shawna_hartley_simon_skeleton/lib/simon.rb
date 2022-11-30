38498e3b8847d7d6520bf2157f4fa7000d5a55b2
“Shawna <shawna.e.hartley@gmail.com>
2022-11-30 00:36:29 -0500

    progress on simon

--- W4D3/shawna_hartley_simon_skeleton/lib/simon.rb
+++ W4D3/shawna_hartley_simon_skeleton/lib/simon.rb
@@ -4,19 +4,36 @@ class Simon
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
 
   end
 
   def play
+    if !game_over 
+      self.take_turn
+    else
+      self.game_over_message
+      self.reset_game
+    end
 
   end
-
+  
   def take_turn
-
+    self.show_sequence
+    self.require_sequence
+  
+    if !game_over
+      self.round_success_message
+      if sequence_length != nil
+        sequence_length += 1
+      end
+    end 
   end
 
   def show_sequence
-
+    self.add_random_color
   end
 
   def require_sequence
@@ -24,7 +41,8 @@ class Simon
   end
 
   def add_random_color
-
+    colors = ["red", "blue", "yellow", "green"]
+    @seq << colors.shuffle.first
   end
 
   def round_success_message
@@ -36,6 +54,9 @@ class Simon
   end
 
   def reset_game
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
 
   end
 end
